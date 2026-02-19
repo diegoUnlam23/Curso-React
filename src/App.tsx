ec1dab3966852222e3e175e4dfc2b46a21ebdcab
Diego <dmsanchez204@gmail.com>
2026-02-19 19:36:39 -0300

    Add Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import './index.css'
-import { Header, Hero, Features, MoreInformation } from './components'
+import "./index.css";
+import { Header, Hero, Features, MoreInformation } from "./components";
 
 function App() {
-
   return (
-    <main className=' w-93.75 m-auto'>
-      <section className='bg-Navy-850 p-6 h-105 relative'>
-        <Header/>
-        <img 
-          className='relative z-10 mt-12' 
-          src="/public/images/illustration-intro.png" 
-          alt="intro-illustration" />
-        <img 
-          className='absolute bottom-0 left-0' 
-          src="/public/images/bg-curvy-mobile.svg" 
-          alt="bg curvy mobile" />
+    <main className=" w-93.75 m-auto">
+      <section className="bg-Navy-850 p-6 h-105 relative">
+        <Header />
+        <img
+          className="relative z-10 mt-12"
+          src="/public/images/illustration-intro.png"
+          alt="intro-illustration"
+        />
+        <img
+          className="absolute bottom-0 left-0"
+          src="/public/images/bg-curvy-mobile.svg"
+          alt="bg curvy mobile"
+        />
       </section>
-      <section className='bg-Navy-900 p-6'>
-        <Hero/>
-        <Features/>
-        <MoreInformation/>
+      <section className="bg-Navy-900 p-6">
+        <Hero />
+        <Features />
+        <MoreInformation />
       </section>
+      <footer className="bg-Navy-850 p-6">
+        <Footer />
+      </footer>
     </main>
-  )
+  );
 }
 
 export default App;
